e72c09a969a94f57b9d77f9bda0b7c6511478d3e
Gabriel Fusca <gabrielfusca@gmail.com>
2015-07-01 11:26:11 -0300

    Actualización de dockerfile para incluir pika y copiar aplicacion de prueba que expone rest api

--- rest_svr/app.py
+++ rest_svr/app.py
@@ -4,7 +4,7 @@ from flask import request
 app = Flask(__name__)
 @app.route('/notify', methods=['POST'])
 def hello_world():
-    return 
+    return request.form['suscriptor'] 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
