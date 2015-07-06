3c6244849b0d315bb510681fa7a844b813b86ce4
Gabriel Fusca <gabrielfusca@gmail.com>
2015-07-06 12:11:28 -0300

    Se agrega notificación mediante rabbit del mensaje recibido por rest api

--- rest_svr/app.py
+++ rest_svr/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
-from flask import request
+# Demo application that notify a message coming from REST API to a message bus (rabbitmq)
 
-app = Flask(__name__)
+from flask.ext.api import FlaskAPI, status
+from flask import request
+import pika
+
+app = FlaskAPI(__name__)
+msg_bus = '172.17.0.13'
+msg_bus_port = 9672
 @app.route('/notify', methods=['POST'])
-def hello_world():
+def notify():
+	msg = request.form['msg']
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host=msg_bus, port=msg_bus_port))
+	channel = connection.channel()
+	channel.queue_declare(queue='hello')
+	channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=msg.str())
+	print " [x] Sent " + msg.str() + " to " + msg_bus
+	return '', status.HTTP_201_CREATED
+
+connection.close()
     return request.form['suscriptor'] 
 
 if __name__ == '__main__':
