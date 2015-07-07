076bccdb66c9232c44169574b3a7bf059682703e
Gabriel Fusca <gabrielfusca@gmail.com>
2015-07-07 16:35:35 -0300

    Se pasa a postman.py la funcionalidad correspondiente al envio de mensajes a traves de queue de rabbit

--- rest_svr/app.py
+++ rest_svr/app.py
@@ -2,32 +2,16 @@
 
 from flask.ext.api import FlaskAPI, status
 from flask import request
-from ConfigParser import ConfigParser
-import pika
-
+from postman import Postman
 app = FlaskAPI(__name__)
 
 # Msg bus configuration
-config = ConfigParser()
-config.read('config.ini')
-msg_bus = config.get('General','msg_bus_host')
-msg_bus_port = config.get('General', 'msg_bus_port')
-queue = config.get('General', 'msg_bus_queue')
-
-# Send a message to the message bus (rabbitmq)
-def send_message(message):
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host=msg_bus, port=msg_bus_port))
-	channel = connection.channel()
-	channel.queue_declare(queue='hello')
-	channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=msg.str())
-	connection.close()
+postman = Postman('config.ini')
 
 @app.route('/notify', methods=['POST'])
 def notify():
 	msg = request.form['msg']
-	send_message(msg)
+	postman.send_message(msg)
 	return '', status.HTTP_201_CREATED
 
 if __name__ == '__main__':
