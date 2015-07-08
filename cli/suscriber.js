2a5febdf6160baa5e5e2e8483a0068ed2fef6549
Gabriel Fusca <gabrielfusca@gmail.com>
2015-07-08 12:54:02 -0300

    Se parametriza aplicación cliente para que se reciba mensajes del contenedor correspondiente. Se pasa mensaje a string para ver la llegada

--- cli/suscriber.js
+++ cli/suscriber.js
@@ -1,18 +1,23 @@
 var amqp = require('amqp');
+var json_config = require('./config.json');
 
-var connection = amqp.createConnection({ host: 'dev.rabbitmq.com' });
+console.log(json_config);
+var msg_host = json_config.msg_bus_host;
+var msg_queue = json_config.msg_bus_queue;
+
+var connection = amqp.createConnection({ host: msg_host });
 
 // Wait for connection to become established.
 connection.on('ready', function () {
   // Use the default 'amq.topic' exchange
-  connection.queue('my-queue', function (q) {
+  connection.queue(msg_queue, { 'passive': true }, function (q) {
       // Catch all messages
       q.bind('#');
 
       // Receive messages
       q.subscribe(function (message) {
         // Print messages to stdout
-        console.log(message);
+        console.log(message.data.toString());
       });
   });
 });
